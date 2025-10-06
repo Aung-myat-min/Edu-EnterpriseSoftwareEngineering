5f8d6f98ef03b1be2a10b10ec6912b53742fb0e0
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2025-10-06 14:09:02 +0100

    feat: task 2 completed

---
+++ week3/task2.py
@@ -0,0 +1,9 @@
+my_name = "AMM"
+
+while True:
+    name = input("Enter your name: ")
+    if(name == my_name):
+        print("Name matches! Program exist....")
+        break
+    else:
+        print("Name unmatches. Please try again!")
