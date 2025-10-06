5cdc4f07b5755db01737e561d9da23022dac8dd8
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2025-10-06 14:07:04 +0100

    feat: task1 completed

---
+++ week3/task1.py
@@ -0,0 +1,8 @@
+while True:
+    user_input = int(input("Enter a number: "))
+    if(user_input < 0):
+        print("This is a negative number.")
+        continue
+    else:
+        print("You have entered a positive number.\n Program Stop.....")
+        break
